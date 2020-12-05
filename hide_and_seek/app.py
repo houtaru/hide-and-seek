d6dfbe5dd453e86ac98bd067ea53de14d3ba7375
San Cao <caothesan@gmail.com>
2020-12-06 01:11:27 +0700

    Add board ui

--- hide_and_seek/app.py
+++ hide_and_seek/app.py
@@ -4,10 +4,13 @@ from .utils.opt import Opt
 import yaml
 import os
 
+
 def load_config(level):
-    path = './configs/level_{}.yaml'.format(level)
-    opt = yaml.load(open(path, 'r'), yaml.Loader)
+    path = "./configs/level_{}.yaml".format(level)
+    opt = yaml.load(open(path, "r"), yaml.Loader)
+    opt["map"] = os.path.join(opt["map_dir"], "map{}.txt".format(level))
     return opt
+
 
 def run():
     args = Opt().parse()
